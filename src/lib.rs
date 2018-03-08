b80deca69b7875148b00627c6e1cbaf2dee6cc18
ROki1988 <roki.swindler@gmail.com>
2018-03-09 00:42:48 +0900

    remove Box

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,7 @@ extern crate lazy_static;
 extern crate regex;
 extern crate rayon;
 
+use std::fmt;
 use chrono::prelude::*;
 use data_encoding::BASE64;
 use crowbar::{Value, LambdaContext, LambdaResult};
@@ -29,15 +30,53 @@ lazy_static! {
 
 #[derive(Debug)]
 enum LogError {
-    ApacheParseError
+    RegexParseError,
+    UTF8Error(std::string::FromUtf8Error),
+    EncodingError(data_encoding::DecodeError),
+    DateTimeParseError(chrono::ParseError),
+    IntError(std::num::ParseIntError),
+    JsonError(serde_json::error::Error)
 }
 
-impl std::fmt::Display for LogError {
+impl From<std::string::FromUtf8Error> for LogError {
+    fn from(err: std::string::FromUtf8Error) -> LogError {
+        LogError::UTF8Error(err)
+    }
+}
+
+impl From<data_encoding::DecodeError> for LogError {
+    fn from(err: data_encoding::DecodeError) -> LogError {
+        LogError::EncodingError(err)
+    }
+}
+
+impl From<chrono::ParseError> for LogError {
+    fn from(err: chrono::ParseError) -> LogError {
+        LogError::DateTimeParseError(err)
+    }
+}
+
+impl From<std::num::ParseIntError> for LogError {
+    fn from(err: std::num::ParseIntError) -> LogError {
+        LogError::IntError(err)
+    }
+}
+
+impl From<serde_json::error::Error> for LogError {
+    fn from(err: serde_json::error::Error) -> LogError {
+        LogError::JsonError(err)
+    }
+}
+
+impl fmt::Display for LogError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match *self {
-            LogError::ApacheParseError => {
-                write!(f, "FAIL. unmatched pattern.")
-            }
+            LogError::RegexParseError => fmt::Display::fmt(self, f),
+            LogError::UTF8Error(ref err) => fmt::Display::fmt(err, f),
+            LogError::EncodingError(ref err) => fmt::Display::fmt(err, f),
+            LogError::DateTimeParseError(ref err) => fmt::Display::fmt(err, f),
+            LogError::IntError(ref err) => fmt::Display::fmt(err, f),
+            LogError::JsonError(ref err) => fmt::Display::fmt(err, f),
         }
     }
 }
@@ -45,18 +84,23 @@ impl std::fmt::Display for LogError {
 impl std::error::Error for LogError {
     fn description(&self) -> &str {
         match *self {
-            LogError::ApacheParseError => "FAIL. unmatched pattern."
+            LogError::RegexParseError => "FAIL. unmatched pattern.",
+            LogError::UTF8Error(ref err) => err.description(),
+            LogError::EncodingError(ref err) => err.description(),
+            LogError::DateTimeParseError(ref err) => err.description(),
+            LogError::IntError(ref err) => err.description(),
+            LogError::JsonError(ref err) => err.description(),
         }
     }
 }
 
-fn apache_log2json(s: &str) -> Result<serde_json::Value, Box<std::error::Error>> {
-    let xs = RE.captures(s).ok_or(LogError::ApacheParseError)?;
+fn apache_log2json(s: &str) -> Result<serde_json::Value, LogError> {
+    let xs = RE.captures(s).ok_or(LogError::RegexParseError)?;
 
     let time =
         DateTime::parse_from_str(&xs[4], "%d/%b/%Y:%H:%M:%S %:z")
             .or(DateTime::parse_from_str(&xs[4], "%d/%b/%Y:%H:%M:%S %z"))?;
-
+    xs[6].parse::<u32>()?;
     Ok(json!({
          "host": xs[1],
         "ident": xs[2],
@@ -69,7 +113,7 @@ fn apache_log2json(s: &str) -> Result<serde_json::Value, Box<std::error::Error>>
     }))
 }
 
-fn transform_data(data: Vec<u8>) -> std::result::Result<Vec<u8>, Box<std::error::Error>> {
+fn transform_data(data: Vec<u8>) -> std::result::Result<Vec<u8>, LogError> {
     let s = String::from_utf8(data)?;
 
     let r = apache_log2json(s.as_str())?;
@@ -86,8 +130,7 @@ fn transform_data_test() {
 }
 
 fn transform_record(record: &FirehoseRecord) -> TransformationRecord {
-    BASE64.decode(record.data.as_bytes())
-        .map_err::<Box<std::error::Error>, _>(|e| Box::new(e))
+    BASE64.decode(record.data.as_bytes()).map_err(|e| LogError::EncodingError(e))
         .and_then(|x|
             transform_data(x)
                 .map(|x|
